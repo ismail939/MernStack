82b2288a83fdb24ed0d7b334065d0ac6ece3a008
ismail939 <ismailsalama526@gmail.com>
2023-07-17 14:17:40 +0300

    Home is printed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+// home and components
+import Home from "./pages/Home"
 function App() {
   return (
     <div className="App">
-     
+     <BrowserRouter>
+      <div className='pages'>
+        <Routes>
+          <Route
+            path='/'
+            element={<Home/>}
+          />
+        </Routes>
+      </div>
+     </BrowserRouter>
     </div>
   );
 }
